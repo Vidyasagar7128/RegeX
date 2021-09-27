3410d60610d714c2b4eec7d47dca7de47dfc9776
Vidyasagar7128 <77886106+Vidyasagar7128@users.noreply.github.com>
2021-09-27 17:39:13 +0530

    .

--- Regx/Patterns.cs
+++ Regx/Patterns.cs
@@ -10,7 +10,7 @@ namespace Regx
         {
             Console.Write("Enter Name: ");
             string name = Console.ReadLine();
-            string rx = @"^[A-Z]{1}[a-z]{3}$";
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
             Regex rxName = new Regex(rx);
             Console.WriteLine($"Name is: {rxName.Match(name)}");
         }
