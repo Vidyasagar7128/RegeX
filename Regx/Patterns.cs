102a4af0821adfd0d1cd4fae5e748610902d2b35
vidyasagar gaikwad <vidyasagargaikwad7128@gmail.com>
2021-09-27 18:22:38 +0530

    valid Email

--- Regx/Patterns.cs
+++ Regx/Patterns.cs
@@ -9,10 +9,17 @@ namespace Regx
         public void Pattern()
         {
             Console.Write("Enter Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter LastName: ");
             string lastName = Console.ReadLine();
-            string rx = @"^[A-Z]{1}[a-z]{3}$";
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
+            string rxE = @"^[a-z]+[.]?[a-z]+[@]+[a-z]+[.]+[a-z]+[.]?[a-z]+$";
             Regex rxName = new Regex(rx);
-            Console.WriteLine($"Name is: {rxName.Match(lastName)}");
+            Regex rxLast = new Regex(rx);
+            Regex rxEmail = new Regex(rxE);
+            Console.WriteLine($"Name is: {rxName.Match(firstName)} Surname : {rxLast.Match(lastName)} Email : {rxEmail.Match(email)}");
             Pattern();
         }
     }
