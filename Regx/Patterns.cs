1722cad53b4fc7c7d635fcd386f257504b58227d
Vidyasagar7128 <77886106+Vidyasagar7128@users.noreply.github.com>
2021-09-27 17:39:36 +0530

    .

--- Regx/Patterns.cs
+++ Regx/Patterns.cs
@@ -10,7 +10,7 @@ namespace Regx
         {
             Console.Write("Enter Name: ");
             string lastName = Console.ReadLine();
-            string rx = @"^[A-Z]{1}[a-z]{3}$";
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
             Regex rxName = new Regex(rx);
             Console.WriteLine($"Name is: {rxName.Match(lastName)}");
             Pattern();
